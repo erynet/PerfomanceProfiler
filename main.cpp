a91fed3d6634b6b9df33262683a199c8551d3c8d
erynet <ldh668@gmail.com>
2015-02-22 20:12:01 +0900

    일단 제출본

--- main.cpp
+++ main.cpp
@@ -12,6 +12,71 @@
 using namespace std;
 
 ConsoleUtil cu;
+bool isFirstShow;
+
+string TimeStamp(){
+	char c[128];
+	SYSTEMTIME st;
+
+	GetSystemTime(&st);
+	sprintf_s(c, 128, "  %4d/%02d/%02d %02d:%02d:%02d.%03d", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond, st.wMilliseconds);
+
+	return string(c);
+}
+
+void Show(CPUReport *rCPU, GPUReport *rGPU, Win32Report *rWin32){
+	char temp[16];
+	if (isFirstShow){
+		isFirstShow = false;
+		cu.Go(1, 2);
+		cu << "CPU :      °C,        %(        %),        J";
+		cu.Go(1, 3);
+		cu << "GPU :      °C,        %,        %,        %,      Mhz,      Mhz,      Mhz";
+	}
+	cu.Go(0, 1);
+	cu << TimeStamp();
+	cu.CC(1);
+	sprintf_s(temp, 16, "% 3d", rCPU->ThermalInfoOfSocket);
+	cu.Go(8, 2);
+	cu << temp;
+	cu.CC(0);
+	sprintf_s(temp, 16, "% 6.2lf", rWin32->processCpuUsage);
+	cu.Go(17, 2);
+	cu << temp;
+	sprintf_s(temp, 16, "% 6.2lf", rWin32->systemCpuUsage);
+	cu.Go(27, 2);
+	cu << temp;
+	sprintf_s(temp, 16, "% 6.2lf", rCPU->ConsumedJoules);
+	cu.Go(38, 2);
+	cu << temp;
+	
+	cu.CC(1);
+	sprintf_s(temp, 16, "% 3d", rGPU->currentThermalGpu);
+	cu.Go(8, 3);
+	cu << temp;
+	cu.CC(0);
+	sprintf_s(temp, 16, "% 3d", rGPU->loadCore);
+	cu.Go(20, 3);
+	cu << temp;
+	sprintf_s(temp, 16, "% 3d", rGPU->loadMemoryControler);
+	cu.Go(30, 3);
+	cu << temp;
+	sprintf_s(temp, 16, "% 3d", rGPU->loadVideoEngine);
+	cu.Go(40, 3);
+	cu << temp;
+	sprintf_s(temp, 16, "% 4d", rGPU->currentFreqCore / 1000);
+	cu.Go(47, 3);
+	cu << temp;
+	sprintf_s(temp, 16, "% 4d", rGPU->currentFreqMemory / 1000);
+	cu.Go(57, 3);
+	cu << temp;
+	sprintf_s(temp, 16, "% 4d", rGPU->currentFreqShader / 1000);
+	cu.Go(67, 3);
+	cu << temp;
+
+	cu.Go(1, 4);
+	cu << ".";
+}
 
 wchar_t* CharToWChar(const char* pstrSrc){
 	size_t nLen = strlen(pstrSrc) + 1;
@@ -20,8 +85,7 @@ wchar_t* CharToWChar(const char* pstrSrc){
 	return pwstr;
 }
 
-
-int attachProcess(string Image, HANDLE& hProcess){
+int AttachProcess(string Image, HANDLE& hProcess){
 	HANDLE hProcessSnap;
 	PROCESSENTRY32 pe32;
 	int retryCount = 100;
@@ -76,12 +140,9 @@ int attachProcess(string Image, HANDLE& hProcess){
 		}
 		retryCount--;
 		Sleep(500);
-
 	}
 		
 	if (hProcess == NULL){
-		//cout << GetLastError() << endl;
-		// code 5 : Access is Denied
 		if (GetLastError() == 5){
 			cu.Go(0, 0);
 			cu << "Access is Denied";
@@ -92,8 +153,8 @@ int attachProcess(string Image, HANDLE& hProcess){
 		return 1;
 }
 
-
 int main(int argc, char **argv){
+
 	//_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	HANDLE hProcess = NULL;
 	
@@ -117,7 +178,7 @@ int main(int argc, char **argv){
 		return 0;
 	}
 	else if (argc == 2){
-		if (!attachProcess(string(argv[1]), hProcess))
+		if (!AttachProcess(string(argv[1]), hProcess))
 			return 0;
 	}
 	else if (argc >= 3){
@@ -125,7 +186,7 @@ int main(int argc, char **argv){
 		log.open(argv[2]);
 		clog.rdbuf(log.rdbuf());
 		
-		if (!attachProcess(string(argv[1]), hProcess))
+		if (!AttachProcess(string(argv[1]), hProcess))
 			return 0;
 		// 이제 cout 을 어떻게 처리할 것인가가 남아있다.
 	}
@@ -148,18 +209,21 @@ int main(int argc, char **argv){
 
 	if (doLogging) {
 		ss.str(std::string());
-		ss << rCPU->header() << "," << rGPU->header() << "," << rWin32->header() << "\n";
+		ss << "MILLISEC_TIMESTAMP_IN_UTC" << "," << rCPU->header() << "," << rGPU->header() << "," << rWin32->header() << "\n";
 		clog << ss.str();
 	}
 
-	for (int i = 0; i < 100000; i++){
+	isFirstShow = true;
+
+	for (int i = 0; i < 10000; i++){
 		Sleep(500);
 		mWin32->onUpdate(rWin32);
 		mCPU->onUpdate(rCPU);
 		mGPU->onUpdate(rGPU);
+		Show(rCPU, rGPU, rWin32);
 		if (doLogging) {
 			ss.str(std::string());
-			ss << rCPU << "," << rGPU << "," << rWin32;
+			ss << TimeStamp() << "," << rCPU << "," << rGPU << "," << rWin32;
 			clog << ss.str() << endl;
 		}
 	}	
